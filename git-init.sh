9406ee450935a393c01dc1f422a7cc4d2fda5cb3
NetAcad DEVASC <changeme@example.com>
2022-12-02 11:02:52 +0000

    deployment test of sample app

---
+++ git-init.sh
@@ -0,0 +1,7 @@
+echo "# sample-deployment-app" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ZakariaBoubker/sample-deployment-app.git
+git push -u origin main
